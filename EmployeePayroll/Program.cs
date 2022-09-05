216ddc1a1fd3eac825d87ce1fa9fe2078ee87237
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-05 20:24:53 +0530

    [Shivani] UC7 - Compute Employee Wage

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -7,8 +7,7 @@ namespace EmployeePayroll
         static void Main(string[] args)
         {
             Console.WriteLine( "Welcome to employee Wage");
-            Console.WriteLine("Choose Option\n 1. Check absent present\t 2. Daily emp wage\t 3. part time wage\t 4. check part time using switch case\t 5. calculate Wage For Month\t 6. Calculate wage for month using while loop");
-            int option = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Choose Option\n 1. Check absent present\t 2. Daily emp wage\t 3. part time wage\t 4. check part time using switch case\t 5. calculate Wage For Month\t 6. Calculate wage for month using while loop\n 7. Compute Employee Wage");            int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
@@ -28,6 +27,9 @@ namespace EmployeePayroll
                     break;
                 case 6:
                     WageForMonthWhileLoop.CalculateWage();
+                    break;
+                case 7:
+                    ComputeEmployeeWage.ComputeEmpWage();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
