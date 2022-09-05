ed0b8ed0b5d6de2d4665e41bd0c6aa134a7b50aa
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-05 18:51:16 +0530

    [Shivani] UC4 - calculate wage using switch case

--- EmployeePayroll/PartTimeWageSwitchCase.cs
+++ EmployeePayroll/PartTimeWageSwitchCase.cs
@@ -10,8 +10,7 @@ namespace EmployeePayroll
     {
         public static void CalculateWage()
         {
-            //const int IS_FULL_TIME = 1;
-            //const int IS_PART_TIME = 2;
+
             const int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             Random random = new Random();
@@ -32,7 +31,7 @@ namespace EmployeePayroll
                     break;
             }
             int empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emplyee daily wage including the part time is "+empWage);
+            Console.WriteLine("Emplyee daily wage including the part time is " + empWage);
 
         }
     }
