513e50f0bc7168e70c3ef1a0c1cd0f025e17e716
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-07 18:57:33 +0530

    [Shivani] UC10 - Employee Wage of multiple companies using array

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -7,7 +7,7 @@ namespace EmployeePayroll
         static void Main(string[] args)
         {
             Console.WriteLine( "Welcome to employee Wage");
-            Console.WriteLine("Choose Option\n 1. Check absent present\n 2. Daily emp wage\n 3. part time wage\n 4. check part time using switch case\n 5. calculate Wage For Month\n 6. Calculate wage for month using while loop\n 7. Compute Employee Wage\n 8. Employee wage for multiple company");            
+            Console.WriteLine("Choose Option\n 1. Check absent present\n 2. Daily emp wage\n 3. part time wage\n 4. check part time using switch case\n 5. calculate Wage For Month\n 6. Calculate wage for month using while loop\n 7. Compute Employee Wage\n 8. Employee wage for multiple company\n 9.Compute Employee Wage and Save Total Wage for each Company\n 10. Ability to manage Employee Wage of multiple companies");            
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -36,11 +36,26 @@ namespace EmployeePayroll
                     WageForMultipleCompany.ComputeWaageForMultipleCompany("D-Mart", 20, 20, 100);
                     WageForMultipleCompany.ComputeWaageForMultipleCompany("Reliance", 20, 20, 100);
                     break;
+                case 9:
+                    EmpWageBuilderObject dmart = new EmpWageBuilderObject("D-Mart", 20, 20, 100);
+                    EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 20, 20, 100);
+                    dmart.ComputeEmployeeWage();
+                    Console.WriteLine(dmart.ToString());
+                    reliance.ComputeEmployeeWage();
+                    Console.WriteLine(reliance.ToString());
+                    break;
+                case 10:
+                    EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+                    empWageBuilderArray.addCompanyEmpWage("D-Mart", 20, 2, 10);
+                    empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 4, 20);
+                    empWageBuilderArray.computeEmpWage();
+                    break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
                     break;
 
             }
+            Console.ReadLine();
 
         }
     }
