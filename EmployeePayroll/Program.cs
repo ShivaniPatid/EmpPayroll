92d3bcb92a784f643158c663cf1d64206bbec667
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-08 18:05:40 +0530

    [Shivani] UC11 - Employee Wage using interface approch

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -7,7 +7,7 @@ namespace EmployeePayroll
         static void Main(string[] args)
         {
             Console.WriteLine( "Welcome to employee Wage");
-            Console.WriteLine("Choose Option\n 1. Check absent present\n 2. Daily emp wage\n 3. part time wage\n 4. check part time using switch case\n 5. calculate Wage For Month\n 6. Calculate wage for month using while loop\n 7. Compute Employee Wage\n 8. Employee wage for multiple company\n 9.Compute Employee Wage and Save Total Wage for each Company\n 10. Ability to manage Employee Wage of multiple companies");            
+            Console.WriteLine("Choose Option\n 1. Check absent present\n 2. Daily emp wage\n 3. part time wage\n 4. check part time using switch case\n 5. calculate Wage For Month\n 6. Calculate wage for month using while loop\n 7. Compute Employee Wage\n 8. Employee wage for multiple company\n 9.Compute Employee Wage and Save Total Wage for each Company\n 10. Ability to manage Employee Wage of multiple companies\n 11. Employee Wage for Multiple Company using Interface approach");            
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -49,6 +49,12 @@ namespace EmployeePayroll
                     empWageBuilderArray.addCompanyEmpWage("D-Mart", 20, 2, 10);
                     empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 4, 20);
                     empWageBuilderArray.computeEmpWage();
+                    break;
+                case 11:
+                    EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+                    empWageBuilder.AddCompanyEmpWage("D-Mart", 20, 20, 100);
+                    empWageBuilder.AddCompanyEmpWage("Reliance", 20, 20, 100);
+                    empWageBuilder.ComputeEmpWage();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
